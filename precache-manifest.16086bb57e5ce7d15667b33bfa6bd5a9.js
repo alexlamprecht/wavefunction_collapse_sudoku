57ac4ebf386d25724b6f19a277a2a7451dc21f58
alexlamprecht <alexander.lamprecht@mail.de>
2022-06-30 18:37:47 +0800

    Updates

---
+++ precache-manifest.16086bb57e5ce7d15667b33bfa6bd5a9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "9993a1c1fcc9818890f6",
+    "url": "/wavefunction_collapse_sudoku/static/css/main.21d1bb05.chunk.css"
+  },
+  {
+    "revision": "9993a1c1fcc9818890f6",
+    "url": "/wavefunction_collapse_sudoku/static/js/main.9993a1c1.chunk.js"
+  },
+  {
+    "revision": "c972895fac6c9db70f84",
+    "url": "/wavefunction_collapse_sudoku/static/js/1.c972895f.chunk.js"
+  },
+  {
+    "revision": "5868631581ee53b11537",
+    "url": "/wavefunction_collapse_sudoku/static/js/2.58686315.chunk.js"
+  },
+  {
+    "revision": "bb899d6e5fa0cc2147fe",
+    "url": "/wavefunction_collapse_sudoku/static/js/runtime~main.bb899d6e.js"
+  },
+  {
+    "revision": "5d8447351758b0bf86d5bb31a45de4af",
+    "url": "/wavefunction_collapse_sudoku/static/media/noise.5d844735.png"
+  },
+  {
+    "revision": "7a5f7ccb33d6f1d023ae0069c6c6079f",
+    "url": "/wavefunction_collapse_sudoku/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/wavefunction_collapse_sudoku/precache-manifest.16086bb57e5ce7d15667b33bfa6bd5a9.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/wavefunction_collapse_sudoku/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
+});
